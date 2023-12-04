0e93a3c167a1ed47a19a16de7b69dae4a577ecf3
eriksauerbier <erik.sauerbier@mailbox.org>
2023-12-04 15:49:24 +0100

    v.1.0
    - First Release

---
+++ Pingtest.ps1
@@ -0,0 +1,19 @@
+﻿#Skript zum dauerhaften Pingen eines Hosts, protokolliert die Fehler unter $LogPath
+#V1.0  23.11.2023  Stannek GmbH  N.Kohlmann
+
+$PC = "192.168.10.124"
+$LogPath = "C:\test\ping.log"
+
+while($true) {
+    try {
+
+        test-connection $PC -count 1 -ErrorAction Stop
+
+    }
+
+    catch {
+
+        out-file  -filepath $LogPath -InputObject "Computer $($PC) nicht erreichbar: $(get-date)" -append  
+    }
+    Start-Sleep -Seconds 1
+}
