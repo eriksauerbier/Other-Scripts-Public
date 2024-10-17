76845bd7eee504b792308483c0de9cf274d81398
eriksauerbier <erik.sauerbier@mailbox.org>
2024-10-17 17:52:27 +0200

    v.1.0
    - First Release

---
+++ Add-NewFolder-in-all-Subfolders.ps1
@@ -0,0 +1,15 @@
+﻿# Skript zum anlegen eines Ordner in alle Unterordner
+# Stannek GmbH - v.1.0 - 17.10.2024 - E.Sauerbier
+
+# Parameter
+$PathRoot = "E:\HOME"  
+$NameFolder = "Aufzeichnungen"  
+
+# Unterorder in Root-Pfad auslesen
+$PathSubfolders = Get-ChildItem -path $PathRoot | Where-Object { $_.PSIsContainer }
+
+# Ordner in allen Unterordner anlegen
+$PathSubfolders | ForEach-Object {
+    Write-host $_.FullName
+    New-Item ($_.FullName+"\$NameFolder") -type directory    
+    }
