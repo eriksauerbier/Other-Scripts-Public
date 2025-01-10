386ba5097d55b2377495784dc4cdbc10e5c90763
Erik Sauerbier <erik.sauerbier@mailbox.org>
2025-01-10 09:48:07 +0100

    v.1.0
    - First Release

---
+++ CleanUp-Server2025.ps1
@@ -0,0 +1,9 @@
+﻿# Diese Skript bereinigt Windows Server 2025 Features die nicht benötigt werden
+# Stannek GmbH - v.1.0 - 06.01.2025 - E.Sauerbier
+
+# AzureArc entfernen
+Remove-WindowsCapability -online -Name AzureArcSetup~~~~
+
+# Windows Feedback Hub entfernen
+Get-AppxPackage | ? {$_.Name -like 'Microsoft.WindowsFeedbackHub*'} | Remove-AppxPackage -AllUsers
+Get-AppxProvisionedPackage -Online | ? {$_.DisplayName -Like 'Microsoft.WindowsFeedbackHub'} | Remove-AppxProvisionedPackage -Online
