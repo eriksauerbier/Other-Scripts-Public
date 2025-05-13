f666130748d34960d1775ef137dbb7a93b8bde8d
Erik Sauerbier <erik.sauerbier@mailbox.org>
2025-05-13 13:02:53 +0200

    v.1.0
    - First Release

---
+++ Unblock-Files.ps1
@@ -0,0 +1,14 @@
+﻿# Skript zum entfernen des Internet-Flags auf Dateien
+# Stannek GmbH - v.1.0 - 17.10.2024 - E.Sauerbier
+
+# Parameter
+$PathRoot = "C:\"  
+
+# Unterorder in Root-Pfad auslesen
+$Files = Get-ChildItem -Path $PathRoot -Recurse -File
+
+# Ordner in allen Unterordner anlegen
+$Files | ForEach-Object {
+    Write-host $_.FullName
+    Unblock-File -Path $_.FullName -Verbose
+    }
